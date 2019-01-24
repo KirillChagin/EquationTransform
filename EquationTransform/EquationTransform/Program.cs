42869d27c56b0e7139413e455547247712f4deb3
KirillChagin <kirillchagin@gmail.com>
2019-01-25 00:12:45 +0700

    new project

---
+++ EquationTransform/EquationTransform/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EquationTransform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
